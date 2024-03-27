7f05f4651136b3d73a964a45f01ed379a28f1503
Osweld <38056431+Osweld@users.noreply.github.com>
2024-03-26 21:41:56 -0600

    Add User entity with basic fields and serialization configurations

---
+++ src/main/java/com/chat/socket/chatappbackend/models/entity/User.java
@@ -0,0 +1,27 @@
+package com.chat.socket.chatappbackend.models.entity;
+
+import jakarta.persistence.*;
+
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    @Column(name = "username",nullable = false,length = 15)
+    private String username;
+    @Column(name = "password",nullable = false,length = 60)
+    private String password;
+    @Column(name = "email",nullable = false,length = 50)
+    private String email;
+    @Column(name = "is_active",nullable = false)
+    private Boolean isActive;
+
+}
