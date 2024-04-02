b2868eb3e3400a9d491057f5a3ede5079c8b485d
Osweld <38056431+Osweld@users.noreply.github.com>
2024-04-01 20:34:16 -0600

    Add Chat entity with fiels

--- src/main/java/com/chat/socket/chatappbackend/models/entity/Chat.java
+++ src/main/java/com/chat/socket/chatappbackend/models/entity/Chat.java
@@ -1,11 +1,20 @@
 package com.chat.socket.chatappbackend.models.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.Objects;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Entity
 @Table(name = "chats")
 public class Chat implements Serializable {
@@ -21,4 +30,20 @@ public class Chat implements Serializable {
     private String name;
     @Column(name = "update_at")
     private LocalDate updateAt;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Chat chat = (Chat) o;
+        return getId() != null && Objects.equals(getId(), chat.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
