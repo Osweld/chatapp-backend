bdb0ffd7e25e1fbf56feb42bfa03f35bbe9f4ecb
Osweld <38056431+Osweld@users.noreply.github.com>
2024-04-01 20:33:53 -0600

    Add User entity with fiels

--- src/main/java/com/chat/socket/chatappbackend/models/entity/User.java
+++ src/main/java/com/chat/socket/chatappbackend/models/entity/User.java
@@ -1,11 +1,19 @@
 package com.chat.socket.chatappbackend.models.entity;
 
 import jakarta.persistence.*;
-
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.Objects;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Entity
 @Table(name = "users")
 public class User implements Serializable {
@@ -14,6 +22,7 @@ public class User implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user")
     private Long id;
     @Column(name = "username",nullable = false,length = 15)
     private String username;
@@ -24,4 +33,19 @@ public class User implements Serializable {
     @Column(name = "is_active",nullable = false)
     private Boolean isActive;
 
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        User user = (User) o;
+        return getId() != null && Objects.equals(getId(), user.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
