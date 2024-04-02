5259874a3118365d1a4623d2144eeaf1a96878cc
Osweld <38056431+Osweld@users.noreply.github.com>
2024-04-01 20:33:41 -0600

    Add Message entity with fiels

---
+++ src/main/java/com/chat/socket/chatappbackend/models/entity/Message.java
@@ -0,0 +1,56 @@
+package com.chat.socket.chatappbackend.models.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "messages")
+public class Message implements Serializable {
+
+    private static final long  SerialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_message")
+    private Long id;
+    @Column(name = "message",nullable = false,length = 250)
+    private String message;
+    @Column(name = "timestamp", nullable = false)
+    private LocalDate timestamp;
+    @Column(name = "is_read",nullable = false)
+    private Boolean isRead;
+    @ManyToOne()
+    @JoinColumn(name = "from_id_user")
+    private User from;
+    @ManyToOne
+    @JoinColumn(name = "id_chat")
+    private Chat chat;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Message message = (Message) o;
+        return getId() != null && Objects.equals(getId(), message.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+}
