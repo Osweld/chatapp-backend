341ccdaf16901f3e9b37ccac046ae15859ea7312
Osweld <38056431+Osweld@users.noreply.github.com>
2024-04-01 20:10:36 -0600

    Add Chat entity with fields and serialization

---
+++ src/main/java/com/chat/socket/chatappbackend/models/entity/Chat.java
@@ -0,0 +1,23 @@
+package com.chat.socket.chatappbackend.models.entity;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "chats")
+public class Chat implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name", length = 100)
+    private String name;
+    @Column(name = "update_at")
+    private LocalDate updateAt;
+}
