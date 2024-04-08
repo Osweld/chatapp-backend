2c21cb53c9d5537225709fe4bb1c733a28c40cc5
Osweld <38056431+Osweld@users.noreply.github.com>
2024-04-08 17:31:04 -0600

    add swagger and security config

--- src/main/java/com/chat/socket/chatappbackend/ChatappBackendApplication.java
+++ src/main/java/com/chat/socket/chatappbackend/ChatappBackendApplication.java
@@ -2,12 +2,19 @@ package com.chat.socket.chatappbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class ChatappBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ChatappBackendApplication.class, args);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
