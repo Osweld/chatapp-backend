faba2c107cb5446ce7e330e71ca38be71dc90d72
Osweld <38056431+Osweld@users.noreply.github.com>
2024-04-08 18:19:39 -0600

    add email config

--- src/main/java/com/chat/socket/chatappbackend/controllers/UserController.java
+++ src/main/java/com/chat/socket/chatappbackend/controllers/UserController.java
@@ -2,15 +2,15 @@ package com.chat.socket.chatappbackend.controllers;
 
 import com.chat.socket.chatappbackend.models.dto.UserCreationDTO;
 import com.chat.socket.chatappbackend.models.dto.UserResponseDTO;
+import com.chat.socket.chatappbackend.models.projections.UserResponse;
 import com.chat.socket.chatappbackend.services.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/user")
@@ -26,5 +26,12 @@ public class UserController {
     @Operation(summary = "Register user")
     public ResponseEntity<UserResponseDTO> userRegistration(@Valid @RequestBody UserCreationDTO userCreationDTO){
        return new ResponseEntity<>(userService.registerUser(userCreationDTO), HttpStatus.CREATED);
+    }
+
+    @GetMapping()
+    public ResponseEntity<Page<UserResponse>> getAllUserByPagination(
+            @RequestParam(name = "size",defaultValue = "10") int size,
+            @RequestParam(name = "page",defaultValue = "0") int page){
+        return new ResponseEntity<>(userService.getAllUsersByPagination(PageRequest.of(page,size)),HttpStatus.OK);
     }
 }
